026e523f9cebaf03e81d221b7715e92ce5173dd7
Wes Morgan <github@wesmorgan.me>
2022-02-02 12:05:32 -0700

    Feature: expose more namespaces (#1)
    * Ignore build_artifacts.txt file
    
    * Fix GRAALVM_HOME error message in compile script
    
    * Expose some more buddy namespaces
    
    * Update README for new namespaces
    
    * Rename get-bytes-from-engine get-engine-bytes
    
    ...in kdf ns

--- test.clj
+++ test.clj
@@ -6,15 +6,15 @@
   (pods/load-pod "./pod-babashka-buddy")
   (pods/load-pod ["clojure" "-M" "-m" "pod.babashka.buddy"]))
 
-(require '[pod.babashka.buddy.hash :as h])
+(require '[pod.babashka.buddy.core.hash :as h])
 
 (prn (h/sha256 "foo"))
 
-(require '[pod.babashka.buddy.mac :as mac])
+(require '[pod.babashka.buddy.core.mac :as mac])
 
 (prn (mac/hash "foo bar" {:key "mysecretkey" :alg :hmac+sha256}))
 
-(require '[pod.babashka.buddy.nonce :as nonce])
+(require '[pod.babashka.buddy.core.nonce :as nonce])
 
 (prn (nonce/random-bytes 64))
 
@@ -22,7 +22,7 @@
 (def secret (nonce/random-bytes 64))
 
 (require '[clojure.string :as string]
-         '[pod.babashka.buddy.codecs :as codecs])
+         '[pod.babashka.buddy.core.codecs :as codecs])
 
 (let [timestamp (System/currentTimeMillis)
       nonce (nonce/random-bytes 64)
@@ -30,6 +30,27 @@
       payload (pr-str {:nonce nonce-hex :timestamp timestamp})
       signature (codecs/bytes->hex (mac/hash payload {:alg hash-algorithm :key secret}))]
   (prn (string/join "-" [nonce-hex timestamp signature])))
+
+(require '[pod.babashka.buddy.core.kdf :as kdf])
+
+(let [key-bytes (kdf/get-engine-bytes {:alg :hkdf+sha256 :key "supersecret" :salt "sea"
+                                       :info "babashka rocks!" :length 32})
+      key-hex (codecs/bytes->hex key-bytes)]
+  (assert (= "5189451aed99c4acf6c3573f5eec223a13ab0840bb0138c4080ab87bdf7d0ebc"
+             key-hex))
+  (prn key-hex))
+
+(require '[pod.babashka.buddy.sign.jwe :as jwe]
+         '[cheshire.core :as json])
+
+(let [jwt {:name "Babashka" :role "Ruler of your shell"}
+      jwt-json (json/encode jwt)
+      key "abcdefghijklmnopqrstuvwxyzABCDEF"
+      jwe (jwe/encrypt jwt-json key)
+      decrypted-jwe (-> jwe (jwe/decrypt key) codecs/bytes->str)]
+  (prn jwe)
+  (prn decrypted-jwe)
+  (assert (= decrypted-jwe jwt-json)))
 
 (when-not (= "executable" (System/getProperty "org.graalvm.nativeimage.kind"))
   (shutdown-agents)
